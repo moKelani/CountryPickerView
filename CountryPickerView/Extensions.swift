b82a679fe43d3b6a9299ab48ad734729e1ca6843
Mohamed Kelany <mohamed.kelany@outlook.com>
2021-06-07 01:01:34 +0200

    change image size

--- CountryPickerView/Extensions.swift
+++ CountryPickerView/Extensions.swift
@@ -54,7 +54,7 @@ extension String {
         UIColor.white.set()
         let rect = CGRect(origin: .zero, size: size)
         UIRectFill(CGRect(origin: .zero, size: size))
-        (self as AnyObject).draw(in: rect, withAttributes: [.font: UIFont.systemFont(ofSize: 29)])
+        (self as AnyObject).draw(in: rect, withAttributes: [.font: UIFont.systemFont(ofSize: 16)])
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         return image
